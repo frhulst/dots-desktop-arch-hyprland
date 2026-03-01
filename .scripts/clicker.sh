a5c06721c760730df8b9a6b9eb12e8a4f3f3ec4a
Adrian Verhulst <123770262+frhulst@users.noreply.github.com>
2026-03-01 11:09:37 +0100

    Improved clicker.sh

--- .scripts/clicker.sh
+++ .scripts/clicker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Skript na kliknutí levým tlačítkem každých 10 sekund
+MS=${1:-1000} # Default to 1000ms if no argument is provided
+SLEEP_TIME=$(printf "%.3f" "$(echo "$MS / 1000" | bc -l)") # Convert milliseconds to seconds for the sleep command
+echo "Clicking every ${MS}ms. Press Ctrl+C to stop."
 while true; do
     xdotool click 1
-    sleep 10
+    sleep "$SLEEP_TIME"
 done
-
