306e4bfadf1d25ad99425a4ddfeabfbe444993db
Adrian Verhulst <123770262+frhulst@users.noreply.github.com>
2026-03-01 11:15:13 +0100

    Explained clicker.sh

--- .scripts/clicker.sh
+++ .scripts/clicker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Usage: ./clicker.sh {time in ms}
 MS=${1:-1000} # Default to 1000ms if no argument is provided
 SLEEP_TIME=$(printf "%.3f" "$(echo "$MS / 1000" | bc -l)") # Convert milliseconds to seconds for the sleep command
 echo "Clicking every ${MS}ms. Press Ctrl+C to stop."
